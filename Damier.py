ce651719642195238bc2c2e401bea3bc414e49e3
Vidoux <58913190+Vidoux@users.noreply.github.com>
2020-12-16 16:09:49 +0100

    Create Damier.py

---
+++ Damier.py
@@ -0,0 +1,37 @@
+from Coordonnées import *
+
+class Damier:
+
+	def __init__(self,taille):
+		self.taille=taille
+		self.grille= self.__new_grille()
+
+	def set_case_dame(self, coordonnées):
+		self.grille[coordonnées.ligne][coordonnées.colonne]=1
+
+	def get_case(self, coordonnées):
+		return self.grille[coordonnées.ligne][coordonnées.colonne] 
+
+	def toString(self):
+		string =""
+		for i in range(0,self.taille):
+			ligne=""
+			for j in range(0,self.taille):
+				ligne += " " + str(self.grille[i][j])
+			string+=ligne
+			if(i<self.taille-1):
+				string+="\n"
+		return string
+
+	def get_taille(self):
+		return self.taille
+
+	# PRIVATE functions
+	def __new_grille(self):
+		tab=[]
+		for i in range(0,self.taille):
+			ligne=[]
+			for j in range(0,self.taille):
+				ligne.append(0)
+			tab.append(ligne)
+		return tab
