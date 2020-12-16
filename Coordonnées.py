b38045bd7760af6095972e5b308947d671b707e3
Vidoux <58913190+Vidoux@users.noreply.github.com>
2020-12-16 16:09:46 +0100

    Create Coordonnées.py

---
+++ Coordonnées.py
@@ -0,0 +1,7 @@
+class Coordonnées:
+	"""[summary]
+	Permet la création d'une variable contenant des coordonnées sur le damier
+	"""
+	def __init__(self,ligne,colonne):
+		self.ligne=ligne
+		self.colonne=colonne
